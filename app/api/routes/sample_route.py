f6303f69617ddfad5ede29cb29a296b624e26e0e
Azamat Dzabaev <azamatdzabaev310@gmail.com>
2024-05-13 03:47:10 +0300

    added_chat_route

--- app/api/routes/sample_route.py
+++ app/api/routes/sample_route.py
@@ -1,11 +1,33 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from app.api.utils import templates
+from pydantic import BaseModel
+import ollama
 
 router = APIRouter()
+
+
+class ChatMessage(BaseModel):
+    role: str = 'user'
+    content: str
 
 
 @router.get('/', response_class=HTMLResponse)
 def get_sample_page(request: Request):
     return templates.TemplateResponse('chat.html',
                                       context={'request': request})
+
+
+@router.post('/chat')
+def post_chat(chat_messages: list[ChatMessage]):
+    messages = []
+    for chat_message in chat_messages:
+        messages.append(
+            {
+                'role': chat_message.role,
+                'content': chat_message.content,
+            }
+        )
+
+    response = ollama.chat(model='llama3', messages=messages)
+    return {'response': response['message']['content']}
--- app/static/chat.js
+++ app/static/chat.js
@@ -1,3 +1,5 @@
+let chat_history = []
+
 document.getElementById('send-btn').addEventListener('click', sendMessage);
 
 document.getElementById('message-input').addEventListener('keydown', function(event) {
@@ -25,7 +27,28 @@ function displayMessage(message) {
 }
 
 function sendToServer(message) {
-    setTimeout(() => { 
-        displayMessage('Сервер: Эхо "' + message + '"');
-    }, 500);
+    const user_chat_message = {
+        role: 'user',
+        content: message
+    };
+    chat_history.push(user_chat_message);
+
+    fetch(document.URL + "chat", {
+        method: "POST",
+        body: JSON.stringify(chat_history),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+        .then((response) => response.json())
+        .then((json) => {
+            const bot_chat_message = {
+                role: 'assistant',
+                content: json['response'] 
+            };
+            chat_history.push(bot_chat_message);
+            displayMessage(bot_chat_message.content);
+        });
+
+    console.log(chat_history)
 }
